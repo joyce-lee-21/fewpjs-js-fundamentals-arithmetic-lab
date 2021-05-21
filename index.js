5f37041e175211156172295008ed518050a9278e
Joyce Lee <joycelee901@gmail.com>
2021-05-21 08:59:11 -0500

    commit tree 856512ecd24703d04a4499148b2a10747d043374

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 3.1
+let num2 = 20
+let num3 = 18
+let num4 = 7
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1
+let mod = num3 % num4
+let max = Math.max(num1, num2, num3, num4)
